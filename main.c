623be09423a939d36b7ff692f24e09248667f342
Anang-Abr <ananabr2610@gmail.com>
2024-04-28 22:00:41 +0800

    spread the tcp and udp handler to a function

--- main.c
+++ main.c
@@ -50,6 +50,7 @@ typedef struct
     int flow_pkts_payload_max;
     int flow_pkts_payload_tot;
     int flow_pkts_payload_std;
+    int pkt_array[50];
 } FlowInfo;
 
 FlowInfo flows[MAX_FLOWS];
@@ -59,6 +60,16 @@ char protocol = 'O';
 int tcp_flow = 0;
 int udp_flow = 0;
 pcap_dumper_t *pcap_dumper;
+
+void process_packet(const u_char *packet, int payload_offset, int payload_length)
+{
+    printf("Payload: ");
+    for (int i = payload_offset; i < payload_offset + payload_length; ++i)
+    {
+        printf("%02x ", packet[i]);
+    }
+    printf("\n");
+}
 
 int find_flow_index(
     const struct in_addr src_ip,
@@ -84,9 +95,163 @@ int find_flow_index(
     return -1;
 }
 
-void packet_handler(unsigned char *user_data, const struct pcap_pkthdr *pkthdr, const unsigned char *packet)
-{
+void tcp_handler(const struct pcap_pkthdr *pkthdr, const unsigned char *packet){
+    const struct ip *ip_header = (struct ip *)(packet + sizeof(struct ethhdr));
+    const struct ethhdr *eth_header = (struct ethhdr *)packet;
+    const struct tcphdr *tcp_header = (struct tcphdr *)(packet + sizeof(struct ethhdr) + ip_header->ip_hl * 4);
+    char src_ip[INET_ADDRSTRLEN];
+    char dst_ip[INET_ADDRSTRLEN];
+    printf("Packet captured\n");
+    printf("Packet length: %d\n", pkthdr->len);
+    printf("pkthdr length: %ld\n", sizeof(struct pcap_pkthdr));
+    printf("Payload size pcap_pkthdr: %ld\n", pkthdr->len - sizeof(struct pcap_pkthdr));
+    printf("Payload size ethhdr: %ld\n", pkthdr->len - sizeof(struct ethhdr));
+    printf("Payload size ip_header: %ld\n", pkthdr->len - sizeof(ip_header));
+    printf("Payload size ip_header: %ld\n", pkthdr->len - (sizeof(ip_header) + sizeof(struct ethhdr) + sizeof(struct pcap_pkthdr) + sizeof(struct tcphdr)));
+    inet_ntop(AF_INET, &(ip_header->ip_src), src_ip, INET_ADDRSTRLEN);
+    inet_ntop(AF_INET, &(ip_header->ip_dst), dst_ip, INET_ADDRSTRLEN);
+    char *src_eth = ether_ntoa((const struct ether_addr *)eth_header->h_source);
+    char *dst_eth = ether_ntoa((const struct ether_addr *)eth_header->h_dest);
+    printf("Source Host: %s\n", src_eth);
+    printf("Destination Host: %s\n", dst_eth);
+    printf("Layer 2 protocol: %d\n", eth_header->h_proto);
+    printf("Source IP: %s\n", src_ip);
+    printf("Destination Ip: %s\n", dst_ip);
+    printf("TTL : %d\n", ip_header->ip_ttl);
+    printf("TOS: %d\n", ip_header->ip_tos);
 
+    printf("Payload size ip_header: %ld\n", pkthdr->len - (sizeof(struct ip) + sizeof(struct ethhdr) + sizeof(struct pcap_pkthdr) + sizeof(struct tcphdr)));
+    printf("Source Port: %d\n", ntohs(tcp_header->th_sport));
+    printf("Destination Port: %d\n", ntohs(tcp_header->th_dport));
+    printf("TCP Flags: 0x%X\n", tcp_header->th_flags);
+    printf("TCP Window: %02X\n", tcp_header->th_win);
+    printf("TCP checksum:%02X\n", tcp_header->th_sum);
+    printf("TCP urgent pointer: %02X\n", tcp_header->th_urp);
+    printf("Protocol: TCP\n");
+    int flow_index = find_flow_index(ip_header->ip_src, ip_header->ip_dst, ntohs(tcp_header->th_sport), ntohs(tcp_header->th_dport));
+    // new flow generated
+    if (flow_index == -1)
+    {
+        if (flow_count < MAX_FLOWS)
+        {
+            flows[flow_count].src_ip = ip_header->ip_src;
+            flows[flow_count].dst_ip = ip_header->ip_dst;
+            flows[flow_count].src_port = ntohs(tcp_header->th_sport);
+            flows[flow_count].dst_port = ntohs(tcp_header->th_dport);
+            flows[flow_count].packet_count = 1;
+            flows[flow_count].protocol = 'T';
+            flows[flow_count].ts_start = pkthdr->ts.tv_sec;
+            flows[flow_count].ts_last = pkthdr->ts.tv_sec;
+            flows[flow_count].tms_start = pkthdr->ts.tv_usec;
+            flows[flow_count].tms_last = pkthdr->ts.tv_usec;
+            flows[flow_count].fwd_tot = 1;
+            flows[flow_count].bwd_tot = 0;
+            flows[flow_count].flow_ACK_flag_count = 0;
+            flows[flow_count].flow_CWR_flag_count = 0;
+            flows[flow_count].flow_ECE_flag_count = 0;
+            flows[flow_count].flow_FIN_flag_count = 0;
+            flows[flow_count].flow_RST_flag_count = 0;
+            flows[flow_count].flow_SYN_flag_count = 0;
+            flows[flow_count].pkt_array[flows[flow_count].packet_count] = pkthdr->len - sizeof(struct ethhdr);
+            printf("array item: %d\n", (pkthdr->len - 54));
+            switch (tcp_header->th_flags)
+            {
+            case 0x01:
+                printf("FIN");
+                break;
+            case 0x02:
+                printf("SYN");
+                break;
+            case 0x04:
+                printf("RST");
+                break;
+            case 0x08:
+                printf("PSH");
+                break;
+            case 0x10:
+                printf("ACK");
+                break;
+            case 0x20:
+                printf("URG");
+                break;
+            case 0x40:
+                printf("ECE");
+                break;
+            case 0x80:
+                printf("CWR");
+                break;
+            case 0x11:
+                printf("ACKFIN");
+                break;
+            default:
+                printf("DEFAULT:%X", tcp_header->th_flags);
+                break;
+            }
+            printf("timestamp masuknya flow: %ld\n", pkthdr->ts.tv_usec);
+            flow_count++;
+        }
+        else
+        {
+            printf("Max flow count reached. Cannot add a new flow.\n");
+        }
+    }
+
+    // update found flow
+    else
+    {
+        flows[flow_index].packet_count++;
+        flows[flow_index].ts_last = pkthdr->ts.tv_sec;
+        flows[flow_index].tms_last = pkthdr->ts.tv_usec;
+        flows[flow_index].tms_start = flows[flow_index].tms_start;
+        flows[flow_index].pkt_array[flows[flow_count].packet_count] = pkthdr->len - sizeof(struct ethhdr);
+        printf("array item: %ld\n", (pkthdr->len - sizeof(struct ethhdr)));
+        switch (tcp_header->th_flags)
+        {
+        case 0x01:
+            printf("FIN");
+            break;
+        case 0x02:
+            printf("SYN");
+            break;
+        case 0x04:
+            printf("RST");
+            break;
+        case 0x08:
+            printf("PSH");
+            break;
+        case 0x10:
+            printf("ACK");
+            break;
+        case 0x20:
+            printf("URG");
+            break;
+        case 0x40:
+            printf("ECE");
+            break;
+        case 0x80:
+            printf("CWR");
+            break;
+        case 0x11:
+            printf("ACKFIN");
+            break;
+        default:
+            printf("DEFAULT:%X", tcp_header->th_flags);
+            break;
+        }
+        // categorize between forward and backward packet
+        if (flows[flow_index].src_ip.s_addr == ip_header->ip_src.s_addr)
+        {
+            flows[flow_index].fwd_tot++;
+        }
+        else
+        {
+            flows[flow_index].bwd_tot++;
+        }
+    }
+    return;
+}
+
+void udp_handler(const struct pcap_pkthdr *pkthdr, const unsigned char *packet){
     const struct ethhdr *eth_header = (struct ethhdr *)packet;
     const struct ip *ip_header = (struct ip *)(packet + sizeof(struct ethhdr));
     char src_ip[INET_ADDRSTRLEN];
@@ -109,105 +274,60 @@ void packet_handler(unsigned char *user_data, const struct pcap_pkthdr *pkthdr, 
     printf("Destination Ip: %s\n", dst_ip);
     printf("TTL : %d\n", ip_header->ip_ttl);
     printf("TOS: %d\n", ip_header->ip_tos);
+    const struct udphdr *udp_header = (struct udphdr *)(packet + sizeof(struct ethhdr) + ip_header->ip_hl * 4);
+    printf("Source Port: %d\n", ntohs(udp_header->uh_sport));
+    printf("Destination Port: %d\n", ntohs(udp_header->uh_dport));
+    printf("Protocol: UDP\n");
+    int flow_index = find_flow_index(ip_header->ip_src, ip_header->ip_dst, ntohs(udp_header->uh_sport), ntohs(udp_header->uh_dport));
+    // membuat flow baru
+    if (flow_index == -1)
+    {
+        if (flow_count < MAX_FLOWS)
+        {
+            flows[flow_count].src_ip = ip_header->ip_src;
+            flows[flow_count].dst_ip = ip_header->ip_dst;
+            flows[flow_count].src_port = ntohs(udp_header->uh_sport);
+            flows[flow_count].dst_port = ntohs(udp_header->uh_dport);
+            flows[flow_count].packet_count = 1;
+            flows[flow_count].protocol = 'U';
+            flows[flow_count].ts_start = pkthdr->ts.tv_sec;
+            flows[flow_count].ts_last = pkthdr->ts.tv_sec;
+            flows[flow_count].tms_start = pkthdr->ts.tv_usec;
+            flows[flow_count].tms_last = pkthdr->ts.tv_usec;
+            flows[flow_count].fwd_tot = 1;
+            flows[flow_count].bwd_tot = 0;
+            printf("timestamp masuknya flow: %ld\n", pkthdr->ts.tv_usec);
+            flow_count++;
+        }
+        else
+        {
+            printf("Max flow count reached. Cannot add a new flow.\n");
+        }
+    }
+    else
+    {
+        flows[flow_index].packet_count++;
+        flows[flow_index].ts_last = pkthdr->ts.tv_sec;
+        flows[flow_index].tms_last = pkthdr->ts.tv_usec;
+    }
+}
 
-
+void packet_handler(unsigned char *user_data, const struct pcap_pkthdr *pkthdr, const unsigned char *packet)
+{
+    const struct ip *ip_header = (struct ip *)(packet + sizeof(struct ethhdr));
     //PROTOCOL TCP
     if (ip_header->ip_p == IPPROTO_TCP)
     {
-        const struct tcphdr *tcp_header = (struct tcphdr *)(packet + sizeof(struct ethhdr) + ip_header->ip_hl * 4);
-        printf("Payload size ip_header: %ld\n", pkthdr->len - (sizeof(struct ip) + sizeof(struct ethhdr) + sizeof(struct pcap_pkthdr) + sizeof(struct tcphdr)));
-        printf("Source Port: %d\n", ntohs(tcp_header->th_sport));
-        printf("Destination Port: %d\n", ntohs(tcp_header->th_dport));
-        printf("TCP Flags: 0x%X\n", tcp_header->th_flags);
-        printf("TCP Window: %02X\n", tcp_header->th_win);
-        printf("TCP checksum:%02X\n", tcp_header->th_sum);
-        printf("TCP urgent pointer: %02X\n", tcp_header->th_urp);
-        printf("Protocol: TCP\n");
-        int flow_index = find_flow_index(ip_header->ip_src, ip_header->ip_dst, ntohs(tcp_header->th_sport), ntohs(tcp_header->th_dport));
-        //new flow generated
-        if (flow_index == -1)
-        {
-            if (flow_count < MAX_FLOWS)
-            {
-                flows[flow_count].src_ip = ip_header->ip_src;
-                flows[flow_count].dst_ip = ip_header->ip_dst;
-                flows[flow_count].src_port = ntohs(tcp_header->th_sport);
-                flows[flow_count].dst_port = ntohs(tcp_header->th_dport);
-                flows[flow_count].packet_count = 1;
-                flows[flow_count].protocol = 'T';
-                flows[flow_count].ts_start = pkthdr->ts.tv_sec;
-                flows[flow_count].ts_last = pkthdr->ts.tv_sec;
-                flows[flow_count].tms_start = pkthdr->ts.tv_usec;
-                flows[flow_count].tms_last = pkthdr->ts.tv_usec;
-                flows[flow_count].fwd_tot = 1;
-                flows[flow_count].bwd_tot = 0;
-                flows[flow_count].bwd_tot = 0;
-                    printf("timestamp masuknya flow: %ld\n", pkthdr->ts.tv_usec);
-                flow_count++;
-            }
-            else
-            {
-                printf("Max flow count reached. Cannot add a new flow.\n");
-            }
-        }
-
-        // update found flow
-        else
-        {
-            flows[flow_index].packet_count++;
-            flows[flow_index].ts_last = pkthdr->ts.tv_sec;
-            flows[flow_index].tms_last = pkthdr->ts.tv_usec;
-            flows[flow_index].tms_start = flows[flow_index].tms_start;
-
-            //categorize between forward and backward packet
-            if(flows[flow_index].src_ip.s_addr == ip_header->ip_src.s_addr){
-                flows[flow_index].fwd_tot++;
-            }else{
-                flows[flow_index].bwd_tot++;
-            }
-        }
+        tcp_handler(pkthdr, packet);
+        process_packet(packet, 54, pkthdr->len - 54);
+        
     }
 
 
     //PROTOCOL UDP
     else if (ip_header->ip_p == IPPROTO_UDP)
     {
-        const struct udphdr *udp_header = (struct udphdr *)(packet + sizeof(struct ethhdr) + ip_header->ip_hl * 4);
-        printf("Source Port: %d\n", ntohs(udp_header->uh_sport));
-        printf("Destination Port: %d\n", ntohs(udp_header->uh_dport));
-        printf("Protocol: UDP\n");
-        int flow_index = find_flow_index(ip_header->ip_src, ip_header->ip_dst, ntohs(udp_header->uh_sport), ntohs(udp_header->uh_dport));
-        //membuat flow baru
-        if (flow_index == -1)
-        {
-            if (flow_count < MAX_FLOWS)
-            {
-                flows[flow_count].src_ip = ip_header->ip_src;
-                flows[flow_count].dst_ip = ip_header->ip_dst;
-                flows[flow_count].src_port = ntohs(udp_header->uh_sport);
-                flows[flow_count].dst_port = ntohs(udp_header->uh_dport);
-                flows[flow_count].packet_count = 1;
-                flows[flow_count].protocol = 'U';
-                flows[flow_count].ts_start = pkthdr->ts.tv_sec;
-                flows[flow_count].ts_last = pkthdr->ts.tv_sec;
-                flows[flow_count].tms_start = pkthdr->ts.tv_usec;
-                flows[flow_count].tms_last = pkthdr->ts.tv_usec;
-                flows[flow_count].fwd_tot = 1;
-                flows[flow_count].bwd_tot = 0;
-                printf("timestamp masuknya flow: %ld\n", pkthdr->ts.tv_usec);
-                flow_count++;
-            }
-            else
-            {
-                printf("Max flow count reached. Cannot add a new flow.\n");
-            }
-        }
-        else
-        {
-            flows[flow_index].packet_count++;
-            flows[flow_index].ts_last = pkthdr->ts.tv_sec;
-            flows[flow_index].tms_last = pkthdr->ts.tv_usec;
-        }
+        udp_handler(pkthdr, packet);
     }
     packet_count++;
     printf("\n");
@@ -220,9 +340,7 @@ void packet_handler(unsigned char *user_data, const struct pcap_pkthdr *pkthdr, 
 
 /**
  * TODO
- * + Create pcap file base on the date
- * + Mark the as source when there is a SYN flag
-* ? Install zeek flowmeter
+ * ? Install zeek flowmeter
 */
 
 int main(int argc, char **argv)
@@ -291,6 +409,7 @@ int main(int argc, char **argv)
         cJSON_AddNumberToObject(tempJson, "tms_last", flows[i].tms_last);
         cJSON_AddNumberToObject(tempJson, "fwd_tot", flows[i].fwd_tot);
         cJSON_AddNumberToObject(tempJson, "bwd_tot", flows[i].bwd_tot);
+        // cJSON_AddArrayToObject(tempJson, "pkt_array", flows);
         cJSON_AddItemToArray(flowArr, tempJson);
         printf("flow array: %s", cJSON_Print(tempJson));
         printf("\n");
